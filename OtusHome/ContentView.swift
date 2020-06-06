32db35c8ab9f94761e5c14c44adaa0802f3518e9
Mikhail Anikin <timkapim10@gmail.com>
2020-06-07 01:25:26 +0300

    Initial Commit

---
+++ OtusHome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OtusHome
+//
+//  Created by Михаил Аникин on 07.06.2020.
+//  Copyright © 2020 Михаил Аникин. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
